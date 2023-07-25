af71d1136214998f985c0992cb445110cacbee1b
Onda <75018767+ondastudio@users.noreply.github.com>
2023-07-25 11:35:56 +0100

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,55 @@
+const contactModal = document.querySelector(".section_contact");
+let refresh = false;
+
+/**
+ * Mutation Observer Helper function
+ * //developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe
+ * @param {string} sel The DOM selector to watch
+ * @param {object} opt MutationObserver options
+ * @param {function} cb Pass Mutation object to a callback function
+ */
+const Observe = (sel, opt, cb) => {
+  const Obs = new MutationObserver((m) => [...m].forEach(cb));
+  document.querySelectorAll(sel).forEach((el) => Obs.observe(el, opt));
+};
+
+// Detects whenever style attribute (display) from ".section_contact" changes
+Observe(
+  ".section_contact",
+  {
+    attributesList: ["style"], // Only the "style" attribute
+    attributeOldValue: true // Report also the oldValue
+  },
+  (m) => {
+    // If display is "block", add #contact to url
+    if (m.oldValue === "display: none;") {
+      onModalShow();
+      // If display is "none" remove it
+    } else {
+      onModalHide();
+      refresh = false;
+    }
+  }
+);
+
+// Modal opens.
+function onModalShow() {
+  window.history.pushState({}, "", "/#contact");
+}
+
+// Modal hides
+function onModalHide() {
+  // If refreshed with the /#contact url, change url to Homepage
+  if (refresh) window.history.pushState({}, "", "/");
+  else window.history.back();
+}
+
+// If refresh with the /#contact url
+if (window.location.href.indexOf("#contact") > -1) {
+  // Simulate click on the "Get in touch" button to open modal
+  document
+    .querySelector(".button.is-icon.no-border.is-menu.w-inline-block")
+    .click();
+  refresh = true;
+  onModalShow();
+}
